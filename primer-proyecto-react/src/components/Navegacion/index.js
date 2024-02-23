a6f4877e35d67d1d1549eda0eac38a81fccb56e7
montserrat <montserratalfarob@gmail.com>
2024-02-23 01:50:01 -0300

    actualizacion 21/2

--- primer-proyecto-react/src/components/Navegacion/index.js
+++ primer-proyecto-react/src/components/Navegacion/index.js
@@ -1,10 +1,22 @@
 import "./Navegacion.css";
-export default function Navegacion(){
+export default function Navegacion({hoja}){
     return(
-        <div className="d-flex text-center">
-        <p> Rick & Morty</p>
-        <a href="./Characters"> Characters</a>
-        <a href="./Contact"> Contact</a>
-    </div>
-    );
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <h1 className="navbar-brand" >Rick & Morty</h1>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className={hoja === "Characters" ? "nav-link active":"nav-link"} aria-current="page" >Characters
+                        </li>
+                        <li className={hoja === "Contact" ? "nav-link active":"nav-link"} >Contact
+                        </li>
+                    
+                    </ul>
+                </div>
+            </div>
+            </nav>
+    )
 }
